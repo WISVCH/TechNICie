073bf3160777503a60d04771eec4483ac65509e6
Freek van Tienen <freek.v.tienen@gmail.com>
2015-03-25 22:11:42 +0100

    Added test programs for domjudge

---
+++ test_programs/helloworld.java
@@ -0,0 +1,8 @@
+
+class HelloWorld {
+  public static void main(String[] args) {
+    String version = System.getProperty("java.version");
+    System.out.println("Hello World!");
+    System.out.println("Java version: "+version);
+  }
+}
